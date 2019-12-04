38b6c2969c4bb64f068e77fada7f4e2ba25af85f
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2019-12-03 22:19:09 -0300

    Add controllers for plans and your configurations

--- src/app/controllers/PlansController.js
+++ src/app/controllers/PlansController.js
@@ -1,8 +1,81 @@
+import * as Yup from 'yup';
 import Plans from '../models/Plans';
 
 class PlansController {
+  async index(req, res) {
+    const plan = await Plans.findAll();
+
+    return res.json(plan);
+  }
+
   async store(req, res) {
-    return res.json();
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number().required(),
+      price: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validations failed' });
+    }
+
+    const { title, duration, price } = req.body;
+
+    const titleCheck = await Plans.findOne({
+      where: {
+        title,
+      },
+    });
+
+    if (titleCheck) {
+      return res.status(400).json({ error: 'Title already exist' });
+    }
+
+    const plans = await Plans.create({
+      title,
+      duration,
+      price,
+    });
+
+    return res.json(plans);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      duration: Yup.number().integer(),
+      price: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validations failed' });
+    }
+
+    const plan = await Plans.findByPk(req.params.id);
+
+    const { title, duration, price } = req.body;
+
+    const titleCheck = await Plans.findOne({
+      where: {
+        title,
+      },
+    });
+
+    if (titleCheck) {
+      return res.status(400).json({ error: 'Title already exist' });
+    }
+
+    const data = await plan.update({ title, duration, price });
+
+    return res.json(data);
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    await Plans.destroy({ where: { id } });
+
+    return res.json({ Message: 'Plan delete with success' });
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -10,11 +10,15 @@ const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
 
+// Middleware intecepts routes
 routes.use(authAuthorization);
 
 routes.post('/students', StudentsController.store);
 routes.put('/students', StudentsController.update);
 
+routes.get('/plans', PlansController.index);
 routes.post('/plans', PlansController.store);
+routes.put('/plans/:id', PlansController.update);
+routes.delete('/plans/:id', PlansController.delete);
 
 export default routes;
