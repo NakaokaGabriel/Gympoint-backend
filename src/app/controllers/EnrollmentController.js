c4b92a9654bae9bf48a96e865b202f72f565472e
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2019-12-04 22:06:38 -0300

    Add enrollments

--- src/app/controllers/EnrollmentController.js
+++ src/app/controllers/EnrollmentController.js
@@ -1,6 +1,25 @@
+import { parseISO, addMonths } from 'date-fns';
+
+import Plans from '../models/Plans';
+import Enrollments from '../models/Enrollments';
+
 class EnrollmentController {
   async store(req, res) {
-    return res.json();
+    const { student_id, plan_id, start_date } = req.body;
+    const plans = await Plans.findByPk(plan_id);
+
+    const finalDate = addMonths(parseISO(start_date), plans.duration);
+    const totalPrice = plans.price * plans.duration;
+
+    const enrollment = await Enrollments.create({
+      student_id,
+      plan_id,
+      start_date,
+      end_date: finalDate,
+      price: totalPrice,
+    });
+
+    return res.json(enrollment);
   }
 }
 
