de6a7d9a7a34da5b61a3babb249255561e755cdd
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2019-12-09 08:37:20 -0300

    Listening of orders unanswered

--- src/app/controllers/UnansweredController.js
+++ src/app/controllers/UnansweredController.js
@@ -3,7 +3,19 @@ import Students from '../models/Students';
 
 class UnansweredController {
   async index(req, res) {
-    const unanswered = await HelpOrders.findAll();
+    const unanswered = await HelpOrders.findAll({
+      where: {
+        answer: null,
+      },
+      attributes: ['id', 'student_id', 'question', 'answer', 'answer_at'],
+      include: [
+        {
+          model: Students,
+          as: 'student',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     return res.json(unanswered);
   }
