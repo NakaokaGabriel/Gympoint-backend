f5e24b24fb325268b41a1bf91017dc835e45b609
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2019-12-31 13:15:04 -0300

    Delete row to foreign keys

--- src/app/models/Enrollments.js
+++ src/app/models/Enrollments.js
@@ -5,8 +5,6 @@ class Enrollments extends Model {
   static init(sequelize) {
     super.init(
       {
-        student_id: Sequelize.INTEGER,
-        plan_id: Sequelize.INTEGER,
         start_date: Sequelize.DATE,
         end_date: Sequelize.DATE,
         price: Sequelize.DECIMAL,
